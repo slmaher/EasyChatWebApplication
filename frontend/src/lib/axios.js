73dbfc978af74410c420af20779ce3a0cdaae793
Salma Maher <salmamaher867@gmail.com>
2025-06-27 22:05:27 +0300

    fix: use VITE_API_BASE for socket

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-    : import.meta.env.VITE_API_BASE + "/api",
-
+      : import.meta.env.VITE_API_BASE + "/api",
   withCredentials: true,
 });
