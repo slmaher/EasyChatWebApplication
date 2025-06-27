e22ac32ebe977043fe05ed55d096a56ecdc45180
Salma Maher <159587974+slmaher@users.noreply.github.com>
2025-06-27 18:14:58 +0000

    add env and connect frontend to backend
    add env and connect frontend to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,37 +17,23 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-
-
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
-  })
+  }),
 );
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/blocks", blockRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-    
-
-  //go one up to front folder
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
-
 app.get("/", (req, res) => {
   res.send("Backend is working!");
 });
-
-
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,7 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : "/api",
+    : import.meta.env.VITE_API_BASE + "/api",
+
   withCredentials: true,
 });
