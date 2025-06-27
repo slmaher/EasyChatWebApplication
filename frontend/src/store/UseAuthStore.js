7a259083e5e6e5fa2d7e4887ccac8ab60c2f8ef2
Salma Maher <salmamaher867@gmail.com>
2025-06-27 21:35:49 +0300

    fix: update UseAuthStore.js and package-lock.json

--- frontend/src/store/UseAuthStore.js
+++ frontend/src/store/UseAuthStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
-import { useChatStore } from "./useChatStore";
+import { useChatStore } from "./useChatStore.js";
 
 const BASE_URL =
   import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
